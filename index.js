8635b7c0c29a569a8cfe0de8a6aaf821e66b2300
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-21 10:35:26 +0300

    Comlete assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(fun = 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(emphatic){
+    if (emphatic==='*'){
+    return function (){
+        return "You are *a hard worker*!"
+    }}
+     else if(emphatic==="||"){
+        return function(){
+            return "You are ||a dedicated programmer||!"
+        }
+     }
+}
